0f616c596765397df543c497526bada953efe4c4
Demian Ferreiro <epidemian@gmail.com>
2016-06-23 22:13:10 -0300

    Split up tests

--- test/parse-tests.js
+++ test/parse-tests.js
@@ -1,7 +1,5 @@
 let assert = require('assert')
-let {
-  Var, App, Fun, Def, parse, format, reduceProgram,
-} = require('../src/lambda')
+let {Var, App, Fun, Def, parse} = require('../src/lambda')
 
 let assertParse = (str, expectedTerms = [], expectedDefs = {}) => {
   let {terms, defs} = parse(str)
@@ -15,7 +13,9 @@ let assertParseFails = (str, error) => {
   assert.throws(() => parse(str), error)
 }
 
+
 describe('parse()', () => {
+
   it('parses an empty program', () => {
     assertParse('')
   })
@@ -199,132 +199,3 @@ describe('parse()', () => {
 
   it('parses a whole program') // TODO program with more than one def and term
 })
-
-let assertFormat = (str, expected) => {
-  let {terms} = parse(str)
-  assert.equal(terms.length, 1)
-  assert.equal(format(terms[0]), expected)
-}
-
-describe('format()', () => {
-  it('respects operations\' precedence when applying parentheses', () => {
-    assertFormat('λa.a λb.b λc.c λd.d', 'λa.a λb.b λc.c λd.d')
-    assertFormat('(λa.a) (λb.b) (λc.c) λd.d', '(λa.a) (λb.b) (λc.c) λd.d')
-    assertFormat('(((λa.a) λb.b) λc.c) λd.d', '(λa.a) (λb.b) (λc.c) λd.d')
-    assertFormat('λa.a (λb.b (λc.c (λd.d)))', 'λa.a λb.b λc.c λd.d')
-  })
-})
-
-let reduceTerm = (str, options) => {
-  let reductions = reduceProgram(str, options)
-  assert.equal(reductions.length, 1)
-  return reductions[0]
-}
-
-let assertReduce = (expr, expected, options) => {
-  assert.strictEqual(reduceTerm(expr, options).final, expected)
-}
-
-describe('reduceProgram()', () => {
-
-  it('does not reduce simple irreducible expressions', () => {
-    assertReduce('x', 'x')
-    assertReduce('x y', 'x y')
-    assertReduce('λx.x', 'λx.x')
-  })
-
-  it('reduces simple beta-redexes', () => {
-    assertReduce('(λx.x) y', 'y')
-    assertReduce('(λx.x y) z', 'z y')
-    assertReduce('(λx.x y) y', 'y y')
-    assertReduce('(λx.x x) y', 'y y')
-  })
-
-  it('reduces multiple beta-redex', () => {
-    assertReduce('(λx.λy.x y) v w', 'v w')
-  })
-
-  it('reduces a beta-redex that is formed only after a reduction', () => {
-    assertReduce('(λx.x y) (λz.z z)', 'y y')
-  })
-
-  it('does at most maxStep reduction steps', () => {
-    let {totalSteps} = reduceTerm('(λx.x x) (λx.x x)', {maxSteps: 42})
-    assert.equal(42, totalSteps)
-  })
-
-  it('indicates when a reduction terminates', () => {
-    let {terminates} = reduceTerm('(λx.x) y')
-    assert(terminates)
-  })
-
-  it('indicates when a reduction does not terminate', () => {
-    let {terminates} = reduceTerm('(λx.x x) (λx.x x)')
-    assert(!terminates)
-  })
-
-  it('can reduce by eta-conversions', () => {
-    assertReduce('λx.f x', 'λx.f x')
-    assertReduce('λx.f x', 'f', {etaEnabled: true})
-    assertReduce('λs.λz.s z', 'λs.s', {etaEnabled: true})
-    assertReduce('λx.y z x', 'y z', {etaEnabled: true})
-  })
-
-  describe('renaming in substitution (λy.T)[x := S]', () => {
-
-    it('renames when y is free in S and x is free in T', () => {
-      assertReduce('(λx.λy.x y) (y z)', 'λy1.y z y1')
-    })
-
-    it('does not rename if y does not occur free in S but x is free in T',
-      () => {
-        assertReduce('(λx.λy.x y) (w z)', 'λy.w z y')
-      }
-    )
-
-    it('does not rename if y is free in S but x does not occur in T', () => {
-      assertReduce('(λx.λy.y) (y z)', 'λy.y')
-    })
-
-    it('does not rename if y is free in S but x occurs always bound on T',
-      () => {
-        assertReduce('(λx.λy.y λx.x) (y z)', 'λy.y λx.x')
-        assertReduce('(λx.λy.y λx.x x y λx.x) (y z)', 'λy.y λx.x x y λx.x')
-      }
-    )
-
-    it('does not choose a name that makes an inner variable bind to another ' +
-      'abstraction',
-      () => {
-        // In this case, λy.λy1.x y y1 must be renamed, but it cannot choose to
-        // use [y := y1] because it would make the inner y bind to the second
-        // abstraction instead of the first one.
-        assertReduce('(λx.λy.λy1.x y) y', 'λy2.λy1.y y2')
-        // Same thing as above but with higher numbers.
-        assertReduce('(λx.λy4.λy5.x y4) y4', 'λy6.λy5.y4 y6')
-        // Same idea, but here it has to go quite deep to find a new name.
-        assertReduce(
-          '(λx.λy.λy1.λy2.λy3.λy4.x y) y', 'λy5.λy1.λy2.λy3.λy4.y y5'
-        )
-      }
-    )
-
-    it('does not choose a name that collides with free variables', () => {
-      assertReduce('(λx.λy.x y y1) y', 'λy2.y y2 y1')
-    })
-
-    it('does not choose a name that collides in any form', () => {
-      // This test combines the other three criteria. y can't be renamed to y1
-      // or y4 because it would collide with free variables; nor renamed to y2
-      // or y5 because it would make y bind to an inner abstraction; not renamed
-      // to y3 or y6 because they are free in the substitution term.
-      assertReduce(
-        '(λx.λy.λy2.λy5.x y y1 y4) (y y3 y6)', 'λy7.λy2.λy5.y y3 y6 y7 y1 y4'
-      )
-    })
-  })
-})
-// TODO Add a test of a beta-reduction that requires more than one
-// alpha-conversion, like (λx.(λy.x)(λz.x)) (y z)
-// or (λx.λy.x (λz.x)) (y z)
-// TODO Add multi-step reduction tests!
